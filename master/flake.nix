1dac78fafe78faff22567be0f074cbb8d8a8932a
hugosenari <hugosenari@gmail.com>
2022-03-12 02:22:38 -0300

    chore: re index flatdb

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''small/tiny, flatfile, jsonl based, inprogress database for nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-flatdb-master.flake = false;
+  inputs.src-flatdb-master.owner = "enthus1ast";
+  inputs.src-flatdb-master.ref   = "refs/heads/master";
+  inputs.src-flatdb-master.repo  = "flatdb";
+  inputs.src-flatdb-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-flatdb-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-flatdb-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
