ca9c312c233d700774cc9192a8c969b4105c73f1
hugosenari <hugosenari@gmail.com>
2022-05-09 20:37:19 -0300

    chore: re index flatdb

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."flatdb-master".dir   = "master";
-  inputs."flatdb-master".owner = "nim-nix-pkgs";
-  inputs."flatdb-master".ref   = "master";
-  inputs."flatdb-master".repo  = "flatdb";
-  inputs."flatdb-master".type  = "github";
-  inputs."flatdb-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."flatdb-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
